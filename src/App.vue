c0ce9a91ece9f6d0e513304f42da4c29cff6bdbd
Corey Fries <coreyrfries@gmail.com>
2021-08-29 19:16:08 -0700

    update

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
   <div id="app">
     <Nav />
     <router-view />
+    <Footer />
   </div>
 </template>
 
 <script>
 import Nav from "./components/Nav.vue";
+import Footer from "./components/Footer.vue";
 export default {
-  components: { Nav },
+  components: { Nav, Footer },
 };
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,37 @@
+<template>
+  <footer class="w-full h-full flex flex-row bottom-0">
+    <div class="w-[50%] h-20 bg-lightgray">
+      <ul
+        class="
+          h-full
+          flex
+          flew-row
+          justify-between
+          px-5
+          font-secondary
+          uppercase
+        "
+      >
+        <a href="" class="w-full self-center text-center">
+          <li>github</li>
+        </a>
+        <a href="" class="w-full self-center text-center">
+          <li>indeed</li>
+        </a>
+        <a href="" class="w-full self-center text-center">
+          <li>email</li>
+        </a>
+      </ul>
+    </div>
+    <div class="w-[50%] h-20 bg-lightgray"></div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
